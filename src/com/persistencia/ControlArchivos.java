5131cbf71123e59591e27e1e3a1fb29c2c34f5a5
ezar1994 <ezar.147@gmail.com>
2019-05-26 17:16:11 -0500

    Creacion de un ejemplo de vista y otras cosas mas

---
+++ src/com/persistencia/ControlArchivos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.persistencia;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+ 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+ 
+/**
+ *
+ * @author Esteban Arteaga
+ */
+public final class ControlArchivos {
+    
+}
